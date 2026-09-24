4497e07dfa37930b53cf341d85f4d2a06a4ee9c6
karl-andres <karl.o.andres@outlook.com>
2026-09-23 20:00:21 -0400

    Add: decode-ways - submission-1

---
+++ Data Structures & Algorithms/decode-ways/submission-1.py
@@ -0,0 +1,22 @@
+"""
+226
+[0,0,1,1]
+
+"""
+
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        # create dp array with one extra for base case
+        dp = [0] * (len(s) + 1)
+
+        # init base case
+        dp[len(s)] = 1
+
+        # recurse
+        for i in range(len(s) - 1, -1, -1):
+            if 1 <= int(s[i]) <= 9:
+                dp[i] += dp[i + 1]
+            if (i <= len(s) - 2) and (10 <= int(s[i:i + 2]) <= 26):
+                dp[i] += dp[i + 2]
+
+        return dp[0]
